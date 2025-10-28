8e23277cebc8f8d65b35cbbb9c7ab76d9d88efd7
georgekaseri22 <georgekaseri22@gmail.com>
2025-10-28 11:02:11 +0000

    fix: update eslint configuration for better page object pattern support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'playwright'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "playwright"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:playwright/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:playwright/recommended",
+    "prettier",
   ],
   env: { node: true, es2022: true },
-  ignorePatterns: ['dist', 'playwright-report', 'test-results'],
+  ignorePatterns: ["dist", "playwright-report", "test-results"],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'playwright/expect-expect': 'off', // Disabled - we use assertions in page object methods
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "playwright/expect-expect": "off", // Disabled - we use assertions in page object methods
   },
 };
